aa1dd97fc90d7651c4076ee3fd764c437a7e5325
mox692 <55653825+mox692@users.noreply.github.com>
2020-10-26 18:49:32 +0900

    add parser test

--- parser/parser.go
+++ parser/parser.go
@@ -6,6 +6,12 @@ import (
 	"interpt/token"
 )
 
+// parserパッケージでは、lexerで生成されたトークン列から、具体的なASTを作成する。
+
+// perserはparse(token=>AST)の手段を提供するParseProgramメソッドを実装している構造体である。
+// lはlexerのポインタを保有している。
+// curTokenは現在解析を行っているtokenを保有している。
+// peekは次のtokenを保有している。
 type Perser struct {
 	l *lexer.Lexer
 
@@ -13,6 +19,7 @@ type Perser struct {
 	peekToken token.Token
 }
 
+// perserを生成する関数。lexerを引数にとる。
 func New(l *lexer.Lexer) *Perser {
 	p := &Perser{l: l}
 
@@ -22,11 +29,82 @@ func New(l *lexer.Lexer) *Perser {
 	return p
 }
 
+// parserの解析tokenを1つ進める。
+// perserに紐づいているlexer内部のnextToken()メソッドを呼び出すことによに実現させている。
 func (p *Perser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 
+// inputからASTを生成。
 func (p *Perser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.perseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Perser) perseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.perseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Perser) perseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// 次の文字が識別子出なかったらerr
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// todo.....
+	// stmt.Value =
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Perser) peekTokenIs(t token.TokenType) bool {
+	if p.peekToken.Type == t {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Perser) curTokenIs(t token.TokenType) bool {
+	if p.curToken.Type == t {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Perser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,9 +11,9 @@ import (
 // 後半で、1つ1つのstatementに対するテストは別のTestStatementというテスト関数に投げている。
 func TestLetStatements(t *testing.T) {
 	input := `
-	let x = 5
-	let y = 10
-	let foo = 4583
+	let x = 5;
+	let y = 10;
+	let foo = 4583;
 	`
 
 	l := lexer.New(input)
@@ -21,7 +21,7 @@ func TestLetStatements(t *testing.T) {
 
 	program := p.ParseProgram()
 	if program == nil {
-		t.Fatal("parse err !!")
+		t.Fatal("parse err")
 	}
 	// *******************todo: let, 識別し, 式　で長さが3か、入力のlet文が3つで3か
 	if len(program.Statements) != 3 {
@@ -41,10 +41,34 @@ func TestLetStatements(t *testing.T) {
 		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
 			return
 		}
-
 	}
 }
 
-func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
-	return false
+// TestLetStatementsで生成された、1つ1つのletstatementに対するテスト。
+func testLetStatement(t *testing.T, s ast.Statement, ident string) bool {
+
+	// 一番初めのtokenのリテラルがletじゃなかったらerr
+	if s.TokenLiteral() != "let" {
+		t.Errorf("tokenLiteral is not '%s', but  '%s'", "let", s.TokenLiteral())
+	}
+
+	// sがLetStatement型出なかったらerr
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("ast.Statement is not %sstatement, but %Tstatement", "let", s)
+		return false
+	}
+
+	// inputから読み込んだ識別子と、テストテーブルとして用意した識別子が一致しなかったらerror
+	if letStmt.Name.Value != ident {
+		t.Errorf("ident want %s, but got %s", ident, letStmt.Name.Value)
+		return false
+	}
+
+	// これをテストする意義があまりわからない。。
+	if letStmt.Name.TokenLiteral() != ident {
+		t.Errorf("")
+		return false
+	}
+	return true
 }
