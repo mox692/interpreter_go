77dccbf1506894fe3f25f3f16ecf2adaea7ce5fb
mox692 <55653825+mox692@users.noreply.github.com>
2020-10-30 17:19:07 +0900

    add String method

--- ast/ast.go
+++ ast/ast.go
@@ -1,13 +1,22 @@
 package ast
 
-import "interpt/token"
+import (
+	"bytes"
+	"interpt/token"
+)
+
+// astのパッケージ。
+// 文章を各コンポーネントに分けていく。
 
 // Node :astにおけるルートノード。
 // これ以下のノードinterfaceにはこのNodeを保有させることで依存関係を作っている。
 type Node interface {
 	TokenLiteral() string
+	// String()は、各ノードがカバーしている部分のリテラルをstringで返します。
+	String() string
 }
 
+// letやreturnのようなstatementを格納するstruct
 type Statement interface {
 	Node
 	statementnode()
@@ -22,14 +31,72 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var buf bytes.Buffer
+
+	for _, s := range p.Statements {
+		buf.WriteString(s.String())
+	}
+	return buf.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) statementnode()       {}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 type LetStatement struct {
 	Token token.Token
-	Name  *Identifier
-	Value Expression
+	Name  *Identifier // let文に続き識別子
+	Value Expression  // 右辺の式
 }
 
 func (ls *LetStatement) statementnode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var buf bytes.Buffer
+
+	buf.WriteString(ls.Token.Literal + " ")
+	buf.WriteString(ls.Name.Value)
+	buf.WriteString(" = ")
+
+	if ls.Value != nil {
+		// *******************Todo:
+		buf.WriteString(ls.Value.String())
+	}
+
+	buf.WriteString(";")
+
+	return buf.String()
+}
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementnode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var buf bytes.Buffer
+
+	buf.WriteString(rs.Token.Literal + " ")
+
+	if rs.ReturnValue != nil {
+		buf.WriteString(rs.ReturnValue.String())
+	}
+
+	return buf.String()
+}
 
 type Identifier struct {
 	Token token.Token
@@ -38,6 +105,9 @@ type Identifier struct {
 
 func (i *Identifier) identifiermentnode()  {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string {
+	return i.Value
+}
 
 // 自身のtokenを返す
 func (p *Program) TokenLiteral() string {
